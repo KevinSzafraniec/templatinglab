958259f3d9043fb82c21e135c255af10fb79b4fb
kszafraniec1 <89156622+kszafraniec1@users.noreply.github.com>
2024-10-30 12:58:12 -0500

    SolidIngredient, LiquidIngredient, Recipe Class created and Ingredient interface made

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,73 @@
+import java.util.ArrayList;
+
+interface Ingredient{
+    public String getName();
+    public double getQuantity();
+}
+
+class SolidIngredient implements Ingredient{
+    private String name;
+    private double quantity;
+
+    public SolidIngredient(String name, double quantity){
+        this.name = name;
+        this.quantity = quantity;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getQuantity(){
+        return quantity;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+}
+
+class LiquidIngredient implements Ingredient{
+    private String name;
+    private double quantity;
+
+    public LiquidIngredient(String name, double quantity){
+        this.name = name;
+        this.quantity = quantity;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getQuantity(){
+        return quantity;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+}
+
+class Recipe<T extends Ingredient>{
+    private String name;
+    private ArrayList<String> instructions = new ArrayList<>();
+    private ArrayList<T> ingredients = new ArrayList<>();
+
+    public Recipe(String name, ArrayList<String> instructions, ArrayList<T> ingredients){
+        this.name = name;
+        this.instructions = instructions;
+        this.ingredients = ingredients;
+    }
+    public void addIngredient(T ing){
+        ingredients.add(ing);
+    }
+    public String print(){
+
+    }
+
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
