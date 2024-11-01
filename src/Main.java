7883adde023e34aa907c94927242ba82929700f2
kszafraniec1 <89156622+kszafraniec1@users.noreply.github.com>
2024-11-01 13:01:06 -0500

    Worked on Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 interface Ingredient{
-    public String getName();
-    public double getQuantity();
+    String getName();
+    double getQuantity();
 }
 
 class SolidIngredient implements Ingredient{
@@ -19,12 +21,12 @@ class SolidIngredient implements Ingredient{
     public double getQuantity(){
         return quantity;
     }
-    public void setName(String name){
-        this.name = name;
-    }
-    public void setQuantity(int quantity){
-        this.quantity = quantity;
-    }
+//    public void setName(String name){
+//        this.name = name;
+//    }
+//    public void setQuantity(int quantity){
+//        this.quantity = quantity;
+//    }
 }
 
 class LiquidIngredient implements Ingredient{
@@ -41,35 +43,86 @@ class LiquidIngredient implements Ingredient{
     public double getQuantity(){
         return quantity;
     }
-    public void setName(String name){
-        this.name = name;
-    }
-    public void setQuantity(int quantity){
-        this.quantity = quantity;
-    }
+//    public void setName(String name){
+//        this.name = name;
+//    }
+//    public void setQuantity(int quantity){
+//        this.quantity = quantity;
+//    }
 }
 
 class Recipe<T extends Ingredient>{
     private String name;
-    private ArrayList<String> instructions;
+    private String instructions;
     private ArrayList<T> ingredients;
 
-    public Recipe(String name, ArrayList<String> instructions, ArrayList<T> ingredients){
+    public Recipe(String name, String instructions){
         this.name = name;
-        this.instructions = new ArrayList<>();
+        this.instructions = instructions;
         this.ingredients = new ArrayList<>();
     }
     public void addIngredient(T ing){
         ingredients.add(ing);
     }
     public String print(){
-
+        String recpie = "";
+        recpie+=name+"\n";
+        recpie+=instructions+"\n";
+        for (int i =0; i < ingredients.size(); i++){
+            recpie+=ingredients.get(i)+"\n";
+        }
+        return recpie;
     }
 
 }
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int choice = -1;
+        int c2 = -1;
+        String name = "";
+        int qty = 0;
+        String instructions = "";
+        System.out.println("Recipe name: ");
+        name = scan.nextLine();
+        System.out.println("Recipe Instructions: ");
+        instructions = scan.nextLine();
+        Recipe<Ingredient> r = new Recipe<>(name, instructions);
+        System.out.println("""
+                1. Add Ingredient
+                2. List Recipe
+                3. Exit""");
+        choice = scan.nextInt();
+        while (choice!=3){
+            if (choice == 1){
+                System.out.println("""
+                        1. Add Solid Ingredient
+                        2. Add Liquid Ingredient""");
+                c2 = scan.nextInt();
+                if (c2 == 1){
+                    System.out.println("Enter Ingredient Name: ");
+                    name = scan.nextLine();
+                    System.out.println("Enter Quantity Amt: ");
+                    qty = Integer.parseInt(scan.nextLine());
+                    r.addIngredient(new SolidIngredient(name, qty));
+                }
+                else if (c2 == 2){
+                    System.out.println("Enter Ingredient Name: ");
+                    name = scan.nextLine();
+                    System.out.println("Enter Quantity Amt: ");
+                    qty = Integer.parseInt(scan.nextLine());
+                    r.addIngredient(new LiquidIngredient(name, qty));
+                }
+            }
+            if (choice == 2){
+                r.print();
+            }
+            System.out.println("""
+                1. Add Ingredient
+                2. List Recipe
+                3. Exit""");
+            choice = scan.nextInt();
+        }
     }
 }
